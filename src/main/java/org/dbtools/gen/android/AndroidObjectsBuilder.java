a0a10e79061e687e65c537d837e81471e39e5bef
Jeff Campbell <jeffdcamp@gmail.com>
2014-04-09 18:23:14 -0600

    Added encrypt option

--- src/main/java/org/dbtools/gen/android/AndroidObjectsBuilder.java
+++ src/main/java/org/dbtools/gen/android/AndroidObjectsBuilder.java
@@ -14,26 +14,19 @@ public class AndroidObjectsBuilder extends DBObjectsBuilder {
     }
 
     public static void buildAll(String schemaFilename, String baseOutputDir, String basePackageName, boolean injectionSupport, boolean dateTimeSupport) {
+        buildAll(schemaFilename, baseOutputDir, basePackageName, injectionSupport, dateTimeSupport, false);
+    }
+
+    public static void buildAll(String schemaFilename, String baseOutputDir, String basePackageName, boolean injectionSupport, boolean dateTimeSupport, boolean encryptionSupport) {
         DBObjectsBuilder builder = new AndroidObjectsBuilder();
         builder.setXmlFilename(schemaFilename);
         builder.setOutputBaseDir(baseOutputDir);
         builder.setPackageBase(basePackageName);
         builder.setInjectionSupport(injectionSupport);
         builder.setDateTimeSupport(dateTimeSupport);
+        builder.setEncryptionSupport(encryptionSupport);
 
         builder.build();
         System.out.println("Generated [" + builder.getObjectBuilder().getNumberFilesGenerated() + "] files.");
     }
-
-    // todo remove
-    public static void main(String[] args) {
-        String projectDir = "/home/jcampbell/src/ldschurch/android/ldstools/LDSToolsAndroid/";
-        String schemaFilename = projectDir + "src/main/database/schema.xml";
-        boolean injectionSupport = true; // support for CDI (Guice)
-        boolean dateTimeSupport = true; // support for jsr DateTime (Joda Time)
-        String baseOutputDir = projectDir + "src/main/java/org/lds/ldstools/domain";
-        String basePackageName = "org.lds.ldstools.domain";
-        AndroidObjectsBuilder.buildAll(schemaFilename, baseOutputDir, basePackageName, injectionSupport, dateTimeSupport);
-    }
-
 }
